8e0b40e877ff256256ba9b8c518902245b968f69
ollieloney <ollie.loney95@gmail.com>
2020-01-12 14:34:51 +0000

    refreshing gitignore

--- my-app/src/utils/AGGridUtils.js
+++
@@ -1,5 +0,0 @@
-export function onGridSizeChanged(params) {
-    console.log('params', params)
-    // fill out any available space to ensure there are no gaps
-    params.api.sizeColumnsToFit();
-}
